7e875af85d74b2644091a5817e2fb71ee94076d1
Pravin  Rathod <143316584+PravinRathod009@users.noreply.github.com>
2026-05-09 07:21:20 +0530

    Update server.js

--- server/server.js
+++ server/server.js
@@ -12,7 +12,7 @@ const app = express();
 connectDB();
 
 // Security middleware
-app.use(helmet());
+// app.use(helmet());
 // app.use(cors({
 //   origin: process.env.CLIENT_URL || 'http://localhost:5173',
 //   credentials: true
